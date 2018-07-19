21a7d5ef05fba4e131a3c56fa62c5459ae0dc646
Searge <ashikcygan@me.com>
2018-07-19 21:55:16 +0300

    Sololearn

---
+++ Sololearn/functions.py
@@ -0,0 +1,15 @@
+```
+Functional programming is a style of programming that (as the name suggests) is based around functions. 
+A key part of functional programming is higher-order functions. We have seen this idea briefly in the previous lesson on functions as objects. Higher-order functions take other functions as arguments, or return them as results
+```
+
+
+def apply_twice(func, arg):
+   return func(func(arg))
+
+
+def add_five(x):
+   return x + 5
+
+
+print(apply_twice(add_five, 10))
