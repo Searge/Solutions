acc9ade94ae1f763607c8fee689ce8e10394079d
Searge <ashikcygan@gmail.com>
2018-11-30 13:51:45 +0200

    Data Science | Spark K Mean

--- Skillshare/DataScience/exercise/SparkKMeans.py
+++ Skillshare/DataScience/exercise/SparkKMeans.py
@@ -8,30 +8,35 @@ K = 5
 
 # Boilerplate Spark stuff:
 conf = SparkConf().setMaster("local").setAppName("SparkKMeans")
-sc = SparkContext(conf = conf)
+sc = SparkContext(conf=conf)
 
-#Create fake income/age clusters for N people in k clusters
+# Create fake income/age clusters for N people in k clusters
+
+
 def createClusteredData(N, k):
     random.seed(10)
-    pointsPerCluster = float(N)/k
+    pointsPerCluster = float(N) / k
     X = []
-    for i in range (k):
+    for i in range(k):
         incomeCentroid = random.uniform(20000.0, 200000.0)
         ageCentroid = random.uniform(20.0, 70.0)
         for j in range(int(pointsPerCluster)):
-            X.append([random.normal(incomeCentroid, 10000.0), random.normal(ageCentroid, 2.0)])
+            X.append([random.normal(incomeCentroid, 10000.0),
+                      random.normal(ageCentroid, 2.0)])
     X = array(X)
     return X
+
 
 # Load the data; note I am normalizing it with scale() - very important!
 data = sc.parallelize(scale(createClusteredData(100, K)))
 
 # Build the model (cluster the data)
 clusters = KMeans.train(data, K, maxIterations=10,
-        runs=10, initializationMode="random")
+                        runs=10, initializationMode="random")
 
 # Print out the cluster assignments
 resultRDD = data.map(lambda point: clusters.predict(point)).cache()
+# it's important to cache y RDD to make sure it's not computed twice
 
 print("Counts by value:")
 counts = resultRDD.countByValue()
@@ -46,6 +51,7 @@ print(results)
 def error(point):
     center = clusters.centers[clusters.predict(point)]
     return sqrt(sum([x**2 for x in (point - center)]))
+
 
 WSSSE = data.map(lambda point: error(point)).reduce(lambda x, y: x + y)
 print("Within Set Sum of Squared Error = " + str(WSSSE))
