3ab60e780a84514727160213d42ad441bfbcd369
Searge <ashikcygan@gmail.com>
2018-12-06 17:18:16 +0200

    Algorithms on Python | brute force & build_home

--- Algorithms/build_house.py
+++ Algorithms/build_house.py
@@ -2,12 +2,12 @@ import graphics as gr
 # Конспект урока http://judge.mipt.ru/mipt_cs_on_python3/labs/lab4.html
 
 
-def build_house():
+def build_house(window, upper_left_corner, width):
     """Функция, которая рисует дом"""
-    pass
+    height = calculate_height(width)
 
 
-window = gr.GraphWin("Russian game", 100, 100)
-build_house()
+window = gr.GraphWin("Brand new house", 300, 300)
+build_house(window, gr.Point(100, 100), 100)
 
 print("Ура! Дом построен!")
