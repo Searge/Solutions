ff20783cd50069d04773f2615879c7c90818ce01
Searge <ashikcygan@gmail.com>
2018-12-06 16:05:08 +0200

    Algorithms on Python | graphics.py

---
+++ Algorithms/build_house.py
@@ -0,0 +1,12 @@
+import graphics as gr
+
+
+def build_house():
+    """Функция, которая рисует дом"""
+    pass
+
+
+window = gr.GaphWin("Russian game", 100, 100)
+build_house()
+
+print("Ура! Дом построен!")
