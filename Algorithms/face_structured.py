836d8d9e4eb6de6a9702c72012df63841419a1ca
Searge <ashikcygan@gmail.com>
2018-12-06 22:25:14 +0200

    Algorithms on Python | Example of Structured Code :bug: :lipstick: :rotating_light:

--- Algorithms/face_structured.py
+++ Algorithms/face_structured.py
@@ -2,6 +2,7 @@ import graphics as gr
 
 window = gr.GraphWin("Jenkslex and Ganzz project", 400, 400)
 
+
 def draw_left_eye():
     eye = gr.Circle(gr.Point(150, 180), 20)
     pupil = gr.Circle(gr.Point(150, 180), 10)
@@ -12,15 +13,17 @@ def draw_left_eye():
     eye.draw(window)
     pupil.draw(window)
 
+
 def draw_right_eye():
     eye = gr.Circle(gr.Point(250, 180), 14)
-    pupil = gr.Circle(gr.Point(250, 180),  7)
+    pupil = gr.Circle(gr.Point(250, 180), 7)
 
     eye.setFill('red')
     pupil.setFill('black')
 
     eye.draw(window)
     pupil.draw(window)
+
 
 def draw_eyebrows():
     eyebrow1 = gr.Line(gr.Point(100, 120), gr.Point(180, 170))
@@ -35,11 +38,13 @@ def draw_eyebrows():
     eyebrow1.draw(window)
     eyebrow2.draw(window)
 
+
 def draw_face():
     face = gr.Circle(gr.Point(200, 200), 100)
     face.setFill('yellow')
 
     face.draw(window)
+
 
 def draw_mouth():
     mouth = gr.Line(gr.Point(150, 260), gr.Point(250, 260))
@@ -48,9 +53,10 @@ def draw_mouth():
 
     mouth.draw(window)
 
+
 def draw_angry_lecturer():
     draw_face()
-    draw_right_eye()
+    draw_left_eye()
     draw_right_eye()
     draw_eyebrows()
     draw_mouth()
