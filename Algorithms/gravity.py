48db18855a18c3772eb9c4d8f9006f21c99bd355
Searge <ashikcygan@gmail.com>
2018-12-08 17:35:27 +0200

    Algorithms on Python | Gravity >> final

--- Algorithms/gravity.py
+++ Algorithms/gravity.py
@@ -14,6 +14,7 @@ window = gr.GraphWin("Model", size_x, size_y)
 coords = gr.Point(200, 200)
 #  Скорость
 velocity = gr.Point(1, -2)
+acceleration = gr.Point(0, 0)
 
 
 def add(point_1, point_2):
@@ -33,28 +34,33 @@ def sub(point_1, point_2):
     return new_point
 
 
-def clear_window():
-    """ Ф-ция, которая очищает экран, рисуя зеленный квадрат
-        на весь экран.
-    """
-    rectangle = gr.Rectangle(gr.Point(0, 0),
-                             gr.Point(size_x, size_y))
-    rectangle.setFill('green')
-    rectangle.draw(window)
+class window_clear:
+    def clear_window():
+        """ Ф-ция, которая очищает экран, рисуя зеленный квадрат
+            на весь экран.
+        """
+        rectangle = gr.Rectangle(gr.Point(0, 0),
+                                 gr.Point(size_x, size_y))
+        rectangle.setFill('green')
+        rectangle.draw(window)
 
-    sun = gr.Circle(gr.Point(400, 400), 50)
-    sun.setFill('yellow')
-    sun.draw(window)
+        sun = gr.Circle(gr.Point(400, 400), 50)
+        sun.setFill('yellow')
+        sun.draw(window)
 
 
-def draw_ball(coords):
-    """ Процесс отрисовки шарика также лучше вынести на отдельный
-        уровень абстракции и оформить в функцию.
-    """
-    circle = gr.Circle(coords, 10)
-    circle.setFill('red')
+class circle:
+    def move(coords):
+        """ Процесс отрисовки шарика также лучше вынести на отдельный
+            уровень абстракции и оформить в функцию.
+        """
+        circle = gr.Circle(coords, 10)
+        circle.setFill('red')
 
-    circle.draw(window)
+        circle.draw(window)
+    # circle = gr.Circle(gr.Point(size_x, size_y), 10)
+    # circle.setFill('red')
+    # circle.draw(window)
 
 # Сначала мы рассчитываем координаты шарика, а после отрисовываем его
 # в полученных координатах. И так по кругу, пока мы не захотим
@@ -90,19 +96,20 @@ def update_acceleration(ball_coords, center_coords):
     """ движение материальной точки в поле центральных сил
     """
     diff = sub(ball_coords, center_coords)
-    distance_2 = (diff.x ** 2 + diff.y ** 2) ** (3/2)
+    distance_2 = (diff.x ** 2 + diff.y ** 2) ** (3 / 2)
 
     # Данная константа установлена методом научного подгона
     G = 2000
     # Теперь ускорение будет меняться в каждый момент времени
     # в соответствии с законом гравитационного притяжения.
-    return gr.Point(-diff.x*G/distance_2, -diff.y*G/distance_2)
+    return gr.Point(-diff.x * G / distance_2, -diff.y * G / distance_2)
 
 
 while True:
-    clear_window()
+    window_clear.clear_window()
 
-    draw_ball(coords)
+    # draw_ball(coords)
+    circle.move(coords)
 
     acceleration = update_acceleration(coords, gr.Point(400, 400))
 
