77d7c672ea3f9c29fae39b193d10abf02bd998c1
Searge <ashikcygan@gmail.com>
2018-12-10 22:46:20 +0200

    Grokking Algirithms > binary_search fin

--- Grokking/binary_search.py
+++ Grokking/binary_search.py
@@ -1,15 +1,23 @@
 def binary_search(list, item):
-    low = 0
-    high = len(list) - 1
+    """ Получает отсортированный массив и значение.
+        Если значение присутствует в массиве.ю то ф-ция возвращает
+        его позицию.
+        При этом, мы должны следить за тем, в какой части массива
+        проводится поиск.
+    """
+    # Храним границы той части, в которой выполняем поиск:
+    low = 0               # сначала массива и
+    high = len(list) - 1  # до конца
 
-    while low <= high:
-        mid = (low + high)
+    while low <= high:    # пока не сократитися до одного елемента
+        # каждый раз проверяем средний элемент:
+        mid = (low + high) / 2
         guess = list[mid]
         if guess == item:
             return mid
-        elif guess > item:
+        elif guess > item:  # много
             high = mid - 1
-        else:
+        else:               # мало
             low = mid + 1
     return None
 
