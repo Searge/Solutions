cce2574badbab98ee289ba8e6da7d3a925f92eae
Searge <ashikcygan@gmail.com>
2018-12-15 16:25:52 +0200

    Algorithms on Python | Turtle >> My Nested Squares :sparkles:

--- Algorithms/turtle/my_nested_squares.py
+++ Algorithms/turtle/my_nested_squares.py
@@ -1,4 +1,18 @@
 import turtle as t
+from random import *
+
+t.width(3)
+t.speed('fastest')
+screen = t.Screen()
+
+def random_color():
+    """ Random colour for turtle
+        No special statement needed for border
+    """
+    red = random()
+    green = random()
+    blue = random()
+    t.color(red, green, blue)
 
 
 def drawCenteredSquare(center, side):
@@ -8,17 +22,23 @@ def drawCenteredSquare(center, side):
     t.up()
     t.goto(xPoint, yPoint)
     t.down()
+    random_color()
+    # t.begin_fill()
     for i in range(4):
         t.forward(side)
         t.right(90)
+    # t.end_fill()
 
 
 def drawNestedSquares(center, side):
-    if side < 1:
-        return
+    """
+    Drawing nested squares in range from
+    10 to choosen value
+    """
+    for i in range(10, side, 10):
+        drawCenteredSquare(center, i)
+        # drawNestedSquares(center, side + 10)
 
-    drawCenteredSquare(center, side)
-    drawNestedSquares(center, side + 10)
 
-
-drawNestedSquares([0, 0], 20)
+drawNestedSquares([0, 0], 200)
+screen.exitonclick()
