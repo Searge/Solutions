d0391becea650fad62ab42146e6eabe9f5bfe133
Searge <ashikcygan@gmail.com>
2018-12-19 16:11:57 +0200

    Algorithms on Python | Turtle > nested polygons

--- Algorithms/turtle/tinker_polygons.py
+++ Algorithms/turtle/tinker_polygons.py
@@ -1,5 +1,5 @@
 import turtle
-
+import random
 
 def upRange(start, stop, step):
     while start <= stop:
@@ -14,12 +14,21 @@ def downRange(start, stop, step):
 
 
 turtle.shape("turtle")
-sides = 8
-i_start = int(3)
-i_end = int(sides)
-i_inc = int(0)
-for i in range(i_start, i_end):
+turtle.speed(10)
+length = 50
+sides = 10
+i_start = float(3)
+i_end = float(sides)
+i_inc = float(1)
+for i in (i_start <= i_end) and upRange(i_start, i_end, 1) or downRange(i_start, i_end, 1):
+    turtle.color('#%06x' % random.randint(0, 2**24 - 1))
+    position = length / 2
+    xP = 0 - position
+    yP = 0 + position
+    turtle.penup()
+    turtle.goto(xP, yP)
+    turtle.pendown()
     angle = 360 / i
     for count in range(int(i)):
-        turtle.forward(40)
+        turtle.forward(length)
         turtle.right(angle)
