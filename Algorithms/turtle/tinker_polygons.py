05cc71a9a98a887152a44f9f117b2036d188f1a6
Searge <ashikcygan@gmail.com>
2018-12-19 16:22:04 +0200

    Algorithms on Python | Turtle > nested polygons zap

--- Algorithms/turtle/tinker_polygons.py
+++ Algorithms/turtle/tinker_polygons.py
@@ -1,5 +1,6 @@
-import turtle
 import random
+import turtle
+
 
 def upRange(start, stop, step):
     while start <= stop:
@@ -13,22 +14,36 @@ def downRange(start, stop, step):
         start -= abs(step)
 
 
+"""Describe this function...
+"""
+
+
+def nestedPolygons(center):
+    global i, length, sides, position, xP, yP, angle
+    i_start = float(3)
+    i_end = float(sides)
+    i_inc = float(1)
+    position = length / 2
+    xP = center[0] - position
+    yP = center[1] + position
+    turtle.penup()
+    turtle.goto(xP, yP)
+    turtle.pendown()
+    for i in (i_start <= i_end) and upRange(i_start, i_end, 1) or downRange(i_start, i_end, 1):
+        turtle.color('#%06x' % random.randint(0, 2**24 - 1))
+        angle = 360 / i
+        for count in range(int(i)):
+            turtle.forward(length)
+            turtle.right(angle)
+
+
+def Nested(center):
+  for x in range(1, length, 10):
+    nestedPolygons(center)
+
+
 turtle.shape("turtle")
 turtle.speed(10)
 length = 50
 sides = 10
-i_start = float(3)
-i_end = float(sides)
-i_inc = float(1)
-for i in (i_start <= i_end) and upRange(i_start, i_end, 1) or downRange(i_start, i_end, 1):
-    turtle.color('#%06x' % random.randint(0, 2**24 - 1))
-    position = length / 2
-    xP = 0 - position
-    yP = 0 + position
-    turtle.penup()
-    turtle.goto(xP, yP)
-    turtle.pendown()
-    angle = 360 / i
-    for count in range(int(i)):
-        turtle.forward(length)
-        turtle.right(angle)
+nestedPolygons([0, 0])
