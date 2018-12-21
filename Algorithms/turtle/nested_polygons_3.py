98ed13c245a13cfa7b2aafa6c27a984e25277b19
Searge <ashikcygan@gmail.com>
2018-12-21 21:42:55 +0200

    Algorithms on Python | Turtle > nested polygons :zap: 5

---
+++ Algorithms/turtle/nested_polygons_3.py
@@ -0,0 +1,32 @@
+import turtle
+import math
+turtle.shape('turtle')
+sides = 3
+R = 10  # задаем радиус первой окружности
+
+
+def more_agles(sides, length):
+    """ Опеределяем функцию, рисующую многоугольник
+    """
+    angle = 360 / sides
+    while sides > 0:
+
+        turtle.left(angle)
+        turtle.forward(length)
+        sides -= 1
+
+
+while sides < 13:
+    # считаем размер стороны многоугольника (a=2Rsin (360/2n))
+    length = 2 * R * math.sin(math.pi / sides)
+    x = (180 - 360 / sides) / 2
+    turtle.left(x)
+
+    more_agles(sides, length)
+    turtle.right(x)
+    turtle.penup()
+    turtle.forward(10)  # задаем расстояние м/у окружностями
+
+    turtle.pendown()
+    sides += 1
+    R += 10  # раз расстояние м/у окружностями 10, увеличиваем радиус на 10
