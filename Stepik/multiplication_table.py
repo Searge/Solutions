daa1c7864c742243506342899d8143d2a7c9b44a
Searge <ashikcygan@gmail.com>
2018-12-29 21:21:41 +0200

    Stepik | Multiplication table :sparckles:

--- Stepik/multiplication_table.py
+++ Stepik/multiplication_table.py
@@ -16,3 +16,11 @@
 
 a, b, c, d = (int(input()) for _ in range(4))
 
+for up in range(c, d+1):
+    print('\t'+str(up), end='')
+print()
+for x in range(a, b+1):
+    print(x, end='\t')
+    for y in range(c, d+1):
+        print(x * y, end='\t')
+    print()
