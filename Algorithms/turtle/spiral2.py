bd14503d3885b20747c9f5f763d2a98ff7ff1cf1
Searge <ashikcygan@gmail.com>
2019-01-04 17:26:53 +0200

    Algorithm | Spiral 2 :rotating_light:

--- Algorithms/turtle/spiral2.py
+++ Algorithms/turtle/spiral2.py
@@ -1,11 +1,12 @@
-from turtle import *
-from math import *
-color("blue")
-down()
+import turtle as t
+import math as m
+
+t.color("blue")
+t.down()
 for i in range(200):
-    t = i / 20 * pi
-    x = (1 + 5 * t) * cos(t)
-    y = (1 + 5 * t) * sin(t)
-    goto(x, y)
-up()
-done()
+    t = i / 20 * m.pi
+    x = (1 + 5 * t) * m.cos(t)
+    y = (1 + 5 * t) * m.sin(t)
+    t.goto(x, y)
+t.up()
+t.done()
