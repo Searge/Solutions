55b4dd7f9f30c1c8165637c74497c121f5ebf57a
Searge <ashikcygan@gmail.com>
2019-01-04 17:39:27 +0200

    Algorithm | Spiral Trinker :art:

--- Algorithms/turtle/spiral_trinker.py
+++ Algorithms/turtle/spiral_trinker.py
@@ -4,8 +4,8 @@ import turtle
 
 i = 0
 while i <= 200:
-  t = (i / 20) * math.pi
-  x = (1 + 5 * t) * math.cos(t / 180.0 * math.pi)
-  y = (1 + 5 * t) * math.sin(t / 180.0 * math.pi)
-  turtle.goto(x,y)
-  i = i + 1
+    t = (i / 20) * math.pi
+    x = (1 + 5 * t) * math.cos(t)
+    y = (1 + 5 * t) * math.sin(t)
+    turtle.goto(x, y)
+    i = i + 1
