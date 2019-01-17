74d0773779b2bf4b1fa4e73afe40c7c037c7258b
Searge <ashikcygan@gmail.com>
2019-01-17 15:17:21 +0200

    Python4Kids | Procedural Colored Rectangles

--- PythonKids/Tkinter/procedural_rectangles.py
+++ PythonKids/Tkinter/procedural_rectangles.py
@@ -2,10 +2,15 @@ import tkinter as tk
 import random
 
 size = 500
+one_fourth = size / 4
+rect_size = size - one_fourth
 
 root = tk.Tk()
 canvas = tk.Canvas(root, width=size, height=size)
 canvas.pack()
+
+colors = ('#068587', '#46B29D', '#4FB99F',
+          '#F0CA4D', '#E37B40', '#DE5B49')
 
 
 def random_rectangle(width, height):
@@ -14,11 +19,13 @@ def random_rectangle(width, height):
     x2 = x1 + random.randrange(width)
     y2 = y1 + random.randrange(height)
     canvas.create_rectangle(x1, y1,
-                            x2, y2)
+                            x2, y2,
+                            fill=random.choice(colors),
+                            outline='')
     # canvas.mainloop() — створює тільки один чотирикутник
 
 
-for i in range(0, 100):
-    random_rectangle(400, 400)
+for i in range(int(one_fourth)):
+    random_rectangle(rect_size, rect_size)
 
 canvas.mainloop()
