d09e538d9955a4f4fb8f081e9b5a1effe42adadc
Searge <ashikcygan@gmail.com>
2019-01-23 13:27:56 +0200

    Python4Kids | Events :sparkles:

--- PythonKids/Tkinter/events.py
+++ PythonKids/Tkinter/events.py
@@ -18,9 +18,19 @@ canvas.create_polygon(one_sixtieth, one_sixtieth,
 
 
 def move_triangle(event):
-    canvas.move(1, 5, 0)
+    if event.keysym == 'Up':
+        canvas.move(1, 0, -3)
+    elif event.keysym == 'Down':
+        canvas.move(1, 0, 3)
+    elif event.keysym == 'Left':
+        canvas.move(1, -3, 0)
+    else:
+        canvas.move(1, 3, 0)
 
 
-canvas.bind_all('<KeyPress-Return>', move_triangle)
+canvas.bind_all('<KeyPress-Up>', move_triangle)
+canvas.bind_all('<KeyPress-Down>', move_triangle)
+canvas.bind_all('<KeyPress-Left>', move_triangle)
+canvas.bind_all('<KeyPress-Right>', move_triangle)
 
 canvas.mainloop()
