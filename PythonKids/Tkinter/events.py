8c9880b747bdcefeb79ce44a59b30f655a6b3b78
Searge <ashikcygan@gmail.com>
2019-01-23 16:38:32 +0200

    Python4Kids | Events: colors, var, itemconfig

--- PythonKids/Tkinter/events.py
+++ PythonKids/Tkinter/events.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import random
 
 size = 600
 
@@ -8,24 +9,30 @@ three_fourth = size - one_fourth    # 400
 one_sixtieth = size / 60            # 10
 one_tenth = size / 10               # 60
 
+colors = ('#068587', '#46B29D', '#4FB99F',
+          '#F0CA4D', '#E37B40', '#DE5B49')
+
 root = tk.Tk()
 canvas = tk.Canvas(root, width=size, height=size)
 canvas.pack()
 
-canvas.create_polygon(one_sixtieth, one_sixtieth,
-                      one_sixtieth, one_tenth,
-                      one_tenth - 10, 35)
+triangle = canvas.create_polygon(one_sixtieth, one_sixtieth,
+                                 one_sixtieth, one_tenth,
+                                 one_tenth - 10, 35)
 
 
 def move_triangle(event):
     if event.keysym == 'Up':
-        canvas.move(1, 0, -3)
+        canvas.move(triangle, 0, -3)
     elif event.keysym == 'Down':
-        canvas.move(1, 0, 3)
+        canvas.move(triangle, 0, 3)
     elif event.keysym == 'Left':
-        canvas.move(1, -3, 0)
+        canvas.move(triangle, -3, 0)
     else:
-        canvas.move(1, 3, 0)
+        canvas.move(triangle, 3, 0)
+    for i in colors:
+        canvas.itemconfig(triangle,
+                          fill=random.choice(colors))
 
 
 canvas.bind_all('<KeyPress-Up>', move_triangle)
