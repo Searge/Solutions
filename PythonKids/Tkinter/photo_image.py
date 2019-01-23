39c586289acf1f8b61f6991ed304ec83387c32d9
Searge <ashikcygan@gmail.com>
2019-01-23 19:02:55 +0200

    Python4Kids | tk.PhotoImage :sparkles:

--- PythonKids/Tkinter/photo_image.py
+++ PythonKids/Tkinter/photo_image.py
@@ -12,7 +12,26 @@ root = tk.Tk()
 canvas = tk.Canvas(root, width=size, height=size)
 canvas.pack()
 
-cat = tk.PhotoImage(file='cat_face.png')
-canvas.create_image(0, 0, anchor='nw', image=cat)
+cat_face = tk.PhotoImage(file='cat_face.png')
+cat = canvas.create_image(half_size, half_size,
+                          anchor='s', image=cat_face)
+
+
+def move_triangle(event):
+    if event.keysym == 'Up':
+        canvas.move(cat, 0, -3)
+    elif event.keysym == 'Down':
+        canvas.move(cat, 0, 3)
+    elif event.keysym == 'Left':
+        canvas.move(cat, -3, 0)
+    else:
+        canvas.move(cat, 3, 0)
+
+
+canvas.bind_all('<KeyPress-Up>', move_triangle)
+canvas.bind_all('<KeyPress-Down>', move_triangle)
+canvas.bind_all('<KeyPress-Left>', move_triangle)
+canvas.bind_all('<KeyPress-Right>', move_triangle)
+
 
 root.mainloop()
