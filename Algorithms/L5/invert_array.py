64ef57d1f4e985b8b618d4d14446adc27104ecfd
Searge <ashikcygan@gmail.com>
2019-03-06 10:50:32 +0200

    Leson #5 Deviations

--- Algorithms/L5/invert_array.py
+++ Algorithms/L5/invert_array.py
@@ -2,19 +2,41 @@ def invert_array(A: list, N: int):
     """ Розвертаємо масив задом-наперед
         в рамках індексів від 0 до N-1
     """
-    pass
+    for k in range(N):
+        A[k] = A[N-k-1]
 
 
 def test_invert_search():
     A1 = [1, 2, 3, 4, 5]
+    print(A1)
+    invert_array(A1, 5)
+    print(A1)
     if A1 == [5, 4, 3, 2, 1]:
         print('test #1 - ok')
     else:
         print('test #1 - ok')
 
-    A2 = [-1, -2, -3, -4, -5]
-    A3 = [10, 20, 30, 10, 100]
+    A2 = [0, 0, 0, 0, 0, 0, 0, 10]
+    print(A2)
+    invert_array(A2, 8)
+    print(A2)
+    if A2 == [10, 0, 0, 0, 0, 0, 0, 0]:
+        print('test #1 - ok')
+    else:
+        print('test #1 - ok')
 
 
 test_invert_search()
 
+"""
+РОЗВЕРТАЄМО СПИСОК:
+
+>>> A = [1, 2, 3, 4, 5]
+>>> B = [0] * 5
+>>> N = 5
+>>> for k in range(N):
+...     B[k] = A[N-k-1]
+...
+>>> B
+[5, 4, 3, 2, 1]
+"""
