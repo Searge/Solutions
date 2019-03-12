3b45b11c440de6a6a57eba5b556d4ce0f1e5005a
Searge <ashikcygan@gmail.com>
2019-03-12 14:15:24 +0200

    Nested Rectangles | Nested Tuples of Coords

--- Algorithms/rectangles.py
+++ Algorithms/rectangles.py
@@ -6,10 +6,8 @@ window = gr.GraphWin('Rectangles', 300, 300)
 def fractal_rectangle(A, B, C, D, deep=10):
     if deep < 1:
         return
-    gr.Line(gr.Point(*A), gr.Point(*B)).draw(window)
-    gr.Line(gr.Point(*B), gr.Point(*C)).draw(window)
-    gr.Line(gr.Point(*C), gr.Point(*D)).draw(window)
-    gr.Line(gr.Point(*D), gr.Point(*A)).draw(window)
+    for M, N in (A, B), (B, C), (C, D), (D, A):
+        gr.Line(gr.Point(*M), gr.Point(*N)).draw(window)
 
 
 my_rectangle = gr.Rectangle(gr.Point(2, 4), gr.Point(4, 8))
