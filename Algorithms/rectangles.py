611fb3701ab619d56bd346023f8d0b9ff278f3d9
Searge <ashikcygan@gmail.com>
2019-03-12 14:26:18 +0200

    Nested Rectangles | Final depl

--- Algorithms/rectangles.py
+++ Algorithms/rectangles.py
@@ -1,6 +1,7 @@
 import graphics as gr
 
-window = gr.GraphWin('Rectangles', 300, 300)
+window = gr.GraphWin('Rectangles', 900, 900)
+alpha = 0.2
 
 
 def fractal_rectangle(A, B, C, D, deep=10):
@@ -8,9 +9,16 @@ def fractal_rectangle(A, B, C, D, deep=10):
         return
     for M, N in (A, B), (B, C), (C, D), (D, A):
         gr.Line(gr.Point(*M), gr.Point(*N)).draw(window)
+    A1 = (A[0] * (1 - alpha) + B[0] * alpha,
+          A[1] * (1 - alpha) + B[1] * alpha)
+    B1 = (B[0] * (1 - alpha) + C[0] * alpha,
+          B[1] * (1 - alpha) + C[1] * alpha)
+    C1 = (C[0] * (1 - alpha) + D[0] * alpha,
+          C[1] * (1 - alpha) + D[1] * alpha)
+    D1 = (D[0] * (1 - alpha) + A[0] * alpha,
+          D[1] * (1 - alpha) + A[1] * alpha)
+    fractal_rectangle(A1, B1, C1, D1, deep=1)
 
 
-my_rectangle = gr.Rectangle(gr.Point(2, 4), gr.Point(4, 8))
-my_rectangle.draw(window)
-
-line1 = gr.Line(gr.Point(2, 4), gr.Point(4, 8))
+fractal_rectangle((100, 100), (500, 500),
+                  (500, 500), (100, 500))
