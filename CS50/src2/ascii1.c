dc09492839aadf600c175919043b8906669c2027
Searge <ashikcygan@gmail.com>
2019-03-25 14:39:47 +0200

    Перетворення типів

--- CS50/src2/ascii1.c
+++ CS50/src2/ascii1.c
@@ -9,6 +9,7 @@ int main(void)
     string s = get_string("String: ");
     for (int i = 0; i < strlen(s); i++)
     {
+        // НЕЯВНЕ ПЕРЕТВОРЕННЯ
         printf("%c %i\n", s[i], s[i]);
     }
 }
