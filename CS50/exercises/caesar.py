d3a638ec0f5298d6ee616f6ba98e1af23899e364
Searge <ashikcygan@gmail.com>
2019-03-26 19:34:04 +0200

    Caesar :zap:

--- CS50/exercises/caesar.py
+++ CS50/exercises/caesar.py
@@ -3,14 +3,18 @@ k = int(sys.argv[1])
 plaintext = input("plaintext:  ")
 
 
-def caesar(k, message):
-    ciphertext = []
+def caesar(key, message):
+    ciphertext = ""
     for symbol in message:
         if not symbol.isalpha():
-            ciphertext.append(symbol)
+            ciphertext += symbol
             continue
-        ciphertext.append(chr(ord(symbol) + k))
-    print('ciphertext:', "".join(ciphertext))
+        char = ord(symbol) + key
+        if ord('a') > char > ord('Z') or char > ord('z'):
+            char -= 26
+        ciphertext += chr(char)
+
+    print('ciphertext:', ciphertext)
 
 
 caesar(k, plaintext)
