71c0ab3d3d6a41db9585f37449ba08060309ba09
Searge <ashikcygan@gmail.com>
2019-03-27 16:43:55 +0200

    Caesar Final :tada:

--- CS50/exercises/caesar.c
+++ CS50/exercises/caesar.c
@@ -2,14 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
-// #include <stdlib.h>
 
-int key;
-int input_length = 0;
-int Z = 90;
-int a = 97;
-int z = 122;
-int cipher = 0;
 
 int main(int argc, string argv[])
 {
@@ -18,6 +11,7 @@ int main(int argc, string argv[])
         printf("Usage: ./caesar key\n");
         return 1;
     }
+    
     char* p = argv[1];
     while (*p != '\0')
     {
@@ -29,30 +23,27 @@ int main(int argc, string argv[])
         p++;
     }
     
-    key = (int) argv[1];
-
+    const int k = atoi(argv[1]);
     // prompt user for a code to encrypt
     string message = get_string("plaintext:  ");
     printf("ciphertext: ");
-    input_length = strlen(message);
 
-    for (int i = 0; i < input_length; i++)
+    for (int i = 0, n = strlen(message); i < n; i++)
     {
-        cipher = message[i] + key;
-        if (!isalpha(message[i]))
+        if (isupper(message[i]))
         {
-        	printf("%c", message[i]);
+        // Get modulo number and add to appropriate case
+             printf("%c", ((message[i] + k) - 65) % 26 + 65);
         }
-        
-        if (a > cipher > Z || cipher > z)
+        else if (islower(message[i]))
         {
-            printf("%c", cipher - 26);
+            printf("%c", ((message[i] + k) - 97) % 26 + 97);
         }
         else
         {
-            printf("%c", cipher);
+            // return unchanged
+            printf("%c", message[i]);
         }
-        cipher = 0;
     }
     printf("\n");
 }
