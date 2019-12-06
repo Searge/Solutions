4fbf14287208f1c2aa6b5a5902ee821f4065b371
Searge <ashikcygan@gmail.com>
2019-12-07 00:35:12 +0200

    Python :zap:

--- Hexlet/python-basics/binary.py
+++ Hexlet/python-basics/binary.py
@@ -1,12 +1,13 @@
 def binary(number: int):
     """Transit decimal to binary."""
     result: list = []
-    if number == 0:
-        result = ['0']
-    while number != 0:
-        modulo: int = int(number) % 2
+
+    if not number:
+        return '0'
+    while number:
+        modulo: int = number % 2
         result.append(str(modulo))
-        number = int(number) // 2
+        number = number // 2
 
     return "".join(result[::-1])
 
