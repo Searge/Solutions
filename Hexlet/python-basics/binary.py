d201b2d7f6258a1aa832a9755ad520d65eec1ef5
Searge <ashikcygan@gmail.com>
2019-12-07 00:21:26 +0200

    Python :sparkles:

--- Hexlet/python-basics/binary.py
+++ Hexlet/python-basics/binary.py
@@ -1,14 +1,13 @@
-def binarys(number: int):
-    """Transit decimal to binary"""
-    result: list = []
-    while number > 0:
+def binary(number: int):
+    """Transit decimal to binary."""
+    result: list = ['0']
+    while number != 0:
         modulo: int = int(number) % 2
         result.append(str(modulo))
-        number = int(number) // 2 
+        number = int(number) // 2
 
     return "".join(result[::-1])
 
 
 if __name__ == "__main__":
-    print(binarys(2345))
-
+    print(binary(int(input('Your number: '))))
