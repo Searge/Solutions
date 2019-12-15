63ebf666974146c0bfe7d8393f674141d94bb6b2
Searge <ashikcygan@gmail.com>
2019-12-15 09:33:25 +0200

    FizzBuzz :zap:

---
+++ Hexlet/python-basics/challenges/pallindrome.py
@@ -0,0 +1,14 @@
+def is_pallindrome(word: str) -> bool:
+    mid = len(word) // 2
+    end = len(word) - 1
+    first = [word[a] for a in range(mid)]
+    last = [word[z] for z in range(end, mid, -1)]
+
+    if first == last:
+        return True
+    else:
+        return False
+
+
+if __name__ == "__main__":
+    print(is_pallindrome(input('Is pallindrome: ')))
