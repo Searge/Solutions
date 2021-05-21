8a94e51b9ec087711250016a9294ee47d16426a0
Searge <ashikcygan@gmail.com>
2021-05-21 15:24:54 +0300

    Merge sort

--- python/algorithms/sorting/sorting.py
+++ python/algorithms/sorting/sorting.py
@@ -18,6 +18,42 @@ def bubble(array: list) -> list:
     return array
 
 
+def merge_sort(array: list) -> list:
+
+    if len(array) < 2:
+        return array
+
+    # Find the middle point and devide it
+    middle: int = len(array) // 2
+
+    left_list = merge_sort(array[:middle])
+    right_list = merge_sort(array[middle:])
+
+    return list(merge(left_list, right_list))
+
+
+def merge(left_half, right_half):
+
+    merged = []
+
+    while left_half and right_half:
+
+        if left_half[0] < right_half[0]:
+            merged.append(left_half[0])
+            left_half.remove(left_half[0])
+
+        else:
+            merged.append(right_half[0])
+            right_half.remove(right_half[0])
+
+    if left_half:
+        merged += left_half
+    else:
+        merged += right_half
+    return merged
+
+
 if __name__ == '__main__':
     lst = [19, 2, 31, 45, 6, 11, 121, 27]
     print(bubble(lst))
+    print(merge_sort(lst))
