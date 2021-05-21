753c350d45c7b502a945d781ec032b7cc15b004e
Searge <ashikcygan@gmail.com>
2021-05-21 15:45:25 +0300

    Bubble sort

--- python/algorithms/sorting/sorting.py
+++ python/algorithms/sorting/sorting.py
@@ -13,7 +13,8 @@ def bubble(array: list) -> list:
 
             if array[idx] > array[idx + 1]:
                 # Swap right value to left
-                array[idx], array[idx + 1] = array[idx + 1], array[idx]
+                array[idx] = array[idx + 1]
+                array[idx + 1] = array[idx]
 
     return array
 
